2e13a76a0db3cc99121b556a93809f523c69ecc5
Mitchel Cabuloy <mitch@kaijucode.io>
2015-08-09 22:57:58 +0800

    Add homepage

--- dokku_django_gulp/settings.py
+++ dokku_django_gulp/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'dokku_django_gulp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- dokku_django_gulp/urls.py
+++ dokku_django_gulp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^admin/', include(admin.site.urls)),
 ]
