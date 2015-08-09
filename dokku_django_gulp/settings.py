de14db129007b5a406ccb8b535d355af6fb9cc7e
Mitchel Cabuloy <mitch@kaijucode.io>
2015-08-10 00:11:57 +0800

    Configure Gulp and staticfiles

--- dokku_django_gulp/settings.py
+++ dokku_django_gulp/settings.py
@@ -112,3 +112,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = (
     'django.contrib.staticfiles.storage.ManifestStaticFilesStorage')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '_build'),
+)
